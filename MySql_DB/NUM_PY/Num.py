fc5886053c4c79696ec58858f93074aa01b9e7b5
Akash1010 <akashdahal6969@gmail.com>
2025-01-26 22:30:18 +0545

    num py starting

---
+++ MySql_DB/NUM_PY/Num.py
@@ -0,0 +1,8 @@
+import numpy as np
+# Create a 1D array (vector)
+arr = np.array([1, 2, 3, 4])
+print(arr)
+
+# Create a 2D array (matrix)
+arr_2d = np.array([[1, 2], [3, 4]])
+print(arr_2d)
---
+++ MySql_DB/NUM_PY/arrayAdd.py
@@ -0,0 +1,19 @@
+import numpy as np
+
+# Array addition
+arr1 = np.array([1, 2, 3])
+arr2 = np.array([4, 5, 6])
+print(arr1 + arr2)  # Output: [5 7 9]
+
+# Array multiplication
+print(arr1 * 2)  # Output: [2 4 6]
+
+arr =np.array([1,2,3,3,1])
+print(np.sum(arr))
+
+#finding min in array .
+print(np.min(arr))
+
+
+#finding max in array .
+print(np.max(arr))
