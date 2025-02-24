56d20c7e13664ccb50964eeadcf3a2ccb6ec590e
Akash1010 <akashdahal6969@gmail.com>
2025-02-24 22:04:14 +0545

    next and iter

---
+++ next.py
@@ -0,0 +1,8 @@
+tuples = ("apple", "banana", "cherry")
+ites = iter(tuples) 
+#iter creates an iterator from an iterable object.
+
+print(next(ites))
+print(next(ites))
+print(next(ites))
+#next retrieves the next item from the iterator..
